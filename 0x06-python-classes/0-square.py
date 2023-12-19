1b6d90e807b9df7e700e8ea60fb39e70d10306cd
noyl <nouhaylaennadri@gmail.Com>
2023-12-19 07:32:48 -0800

    alx

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Class that defines properties of square.
+    """
+    pas#!/usr/bin/python3
+    """Defines a class Square"""
+
+
+    class Square:
+            """Class that defines properties of square.
+                """
+                    pass
