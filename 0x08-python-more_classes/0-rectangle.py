6a07362fd485363c4868a146d1ec28217ee2562e
noyl <nouhaylaennadri@gmail.Com>
2024-01-05 00:25:56 -0800

    alx

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# 0-rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a Rectangle class."""
+"""0-rectangle, built for Holberton project 0x08 task 0.
+"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
-    pass
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+    passs
